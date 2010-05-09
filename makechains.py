68671f56aa8f3f6b53a919cbea0f8e5551b49f19
Aaron Gallagher <habnabit@gmail.com>
2010-05-08 17:22:04 -0700

    Making makechains actually useful again.

--- makechains.py
+++ makechains.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python
+from infobat.config import conf
 from infobat import chains
 import sys
 
-def main(dbfile):
-    db = chains.Database(dbfile)
+def main(config_file):
+    conf.load(open(config_file))
+    conf.apply_defaults()
+    db = chains.Database(conf['database.dbm'])
     for line in sys.stdin:
         db.learn(line.rstrip('\r\n'))
     db.sync()
