6cf387da102f072ceffde30cbf358c7805cabb4c
Colin Dunklau <colin.dunklau@gmail.com>
2020-02-16 00:45:50 +0100

    Support JSON for web UI's bans endpoint

--- infobob/http.py
+++ infobob/http.py
@@ -1,6 +1,7 @@
 import os.path
 import itertools
 import operator
+import json
 
 from twisted.internet.defer import inlineCallbacks
 from twisted.web import server
@@ -22,6 +23,47 @@ def renderTemplate(request, tmpl, **kwargs):
                   .render('html', doctype='html5', encoding='utf-8'))
     request.finish()
 
+
+def renderJSONBans(request, bans, show_unset, show_recent_expiration):
+    byChannel = {}
+    for channel, channelBans in bans:
+        renderedBans = []
+        for ban in channelBans:
+            (
+                _,
+                mask,
+                mode,
+                set_at,
+                set_by,
+                expire_at,
+                reason,
+                unset_at,
+                unset_by,
+            ) = ban
+            rendered = dict(
+                mask=mask,
+                mode=mode,
+                setBy=set_by.partition('!')[0],
+                setAt=set_at.isoformat(),
+                reason=reason,
+                expiry=dict(
+                    when=expire_at.isoformat() if expire_at else None,
+                    expired=bool(show_recent_expiration),
+                ),
+            )
+            if show_unset:
+                rendered['unset'] = dict(
+                    unsetBy=unset_by.partition('!')[0] if unset_by else None,
+                    unsetAt=unset_at.isoformat() if unset_by else None,
+                )
+            renderedBans.append(rendered)
+        byChannel[channel] = renderedBans
+
+    request.setHeader('Content-type', 'application/json; charset=utf-8')
+    request.write(json.dumps(byChannel, ensure_ascii=True))
+    request.finish()
+
+
 class InfobobWebUI(object):
     app = klein.Klein()
 
@@ -29,13 +71,24 @@ class InfobobWebUI(object):
         self.loader = loader
         self.dbpool = dbpool
 
+    def renderBans(self, request, bans, show_unset, show_recent_expiration):
+        variables = dict(
+            bans=bans,
+            show_unset=show_unset,
+            show_recent_expiration=show_recent_expiration,
+        )
+        if request.getHeader('accept') == 'application/json':
+            renderJSONBans(request, **variables)
+        else:
+            renderTemplate(request, self.loader.load('bans.html'), **variables)
+
     @app.route('/bans')
     @inlineCallbacks
     def bans(self, request):
         bans = yield self.dbpool.get_active_bans()
         bans = itertools.groupby(bans, operator.itemgetter(0))
-        renderTemplate(request, self.loader.load('bans.html'),
-            bans=bans, show_unset=False, show_recent_expiration=False)
+        self.renderBans(
+            request, bans=bans, show_unset=False, show_recent_expiration=False)
 
     @app.route('/bans/expired')
     @app.route('/bans/expired/<int:count>')
@@ -44,16 +97,16 @@ class InfobobWebUI(object):
         bans = yield self.dbpool.get_recently_expired_bans(count)
         bans.sort(key=operator.itemgetter(0, 7))
         bans = itertools.groupby(bans, operator.itemgetter(0))
-        renderTemplate(request, self.loader.load('bans.html'),
-            bans=bans, show_unset=True, show_recent_expiration=True)
+        self.renderBans(
+            request, bans=bans, show_unset=True, show_recent_expiration=True)
 
     @app.route('/bans/all')
     @inlineCallbacks
     def allBans(self, request):
         bans = yield self.dbpool.get_all_bans()
         bans = itertools.groupby(bans, operator.itemgetter(0))
-        renderTemplate(request, self.loader.load('bans.html'),
-            bans=bans, show_unset=True, show_recent_expiration=False)
+        self.renderBans(
+            request, bans=bans, show_unset=True, show_recent_expiration=False)
 
     @app.route('/bans/edit/<rowid>/<auth>', methods=['GET', 'HEAD'])
     @inlineCallbacks
